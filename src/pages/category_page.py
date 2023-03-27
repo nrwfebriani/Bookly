3f85fe03e0bc45d114509605e519a9d6345e2ef4
elzanataliw <elza.natali.w@mail.ugm.ac.id>
2023-03-27 17:04:57 +0700

    Navbar update

---
+++ src/pages/category_page.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from hydralit import HydraApp
+from hydralit import HydraHeadApp
+from genre_page import GenrePage
+from author_page import AuthorPage
+
+class CategoryPage(HydraHeadApp):
+    def run(self):
+        app = HydraApp(title='Bookly')
+        
+        option = st.selectbox('Category', ('by Author','by Genre'))
