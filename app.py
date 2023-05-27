53856f7b49e87c11133327d5322e7e27de96feb0
nrwfebriani <nur.wulan.febriani@mail.ugm.ac.id>
2023-05-27 16:11:17 +0700

    add new page

--- app.py
+++ app.py
@@ -3,38 +3,53 @@ from flask import Flask, request, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("home.html")
 
 
-@app.route('/recommend')
+@app.route("/recommend")
 def recommend():
     return render_template("recommend.html")
 
-@app.route('/category')
+
+@app.route("/category")
 def category():
     return render_template("category.html")
 
-@app.route('/savedbooks')
+
+@app.route("/savedbooks")
 def savedbooks():
     return render_template("savedbooks.html")
 
-@app.route('/profile')
+
+@app.route("/profile")
 def profile():
     return render_template("profile.html")
 
+
+@app.route("/bygenre")
+def bygenre():
+    return render_template("bygenre.html")
+
+
+@app.route("/byauthor")
+def byauthor():
+    return render_template("byauthor.html")
+
+
 # Route for handling the login page logic
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
+    if request.method == "POST":
+        if request.form["username"] != "admin" or request.form["password"] != "admin":
+            error = "Invalid Credentials. Please try again."
         else:
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
+            return redirect(url_for("home"))
+    return render_template("login.html", error=error)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.debug = True
     app.run()
