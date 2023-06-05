9437a9bac4b1183321f7e6182469f4427dfbfeed
nrwfebriani <nur.wulan.febriani@mail.ugm.ac.id>
2023-06-05 22:45:03 +0700

    tidy up

--- src/pages/author_page.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from hydralit import HydraHeadApp
-
-
-class AuthorPage(HydraHeadApp):
-    def run(self):
-        st.title('Categorize by Author')
--- src/pages/saved_page.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-from hydralit import HydraHeadApp
-
-
-class SavedPage(HydraHeadApp):
-    def run(self):
-        st.title('Saved Books')
